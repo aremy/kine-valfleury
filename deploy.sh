ef0ea29b95e0023ff4c147b9c079b6f676ac57b2
remy <aremy>
2018-06-10 21:03:10 +0200

    Fixed ids in footer and use bundle exec in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir -p tmp
 
-jekyll build
+bundle exec jekyll clean
+bundle exec jekyll build
 
 aws s3 cp _site/index.html s3://kine-valfleury.fr --region eu-west-2
 aws s3 cp _site/en/index.html s3://kine-valfleury.fr/en/ --region eu-west-2
