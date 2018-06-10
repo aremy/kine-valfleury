b1b8f0a7bfb0465afa5e0e792afcd326b5d32cd7
remy <aremy>
2018-06-10 18:23:35 +0200

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ jekyll build
 aws s3 cp _site/index.html s3://kine-valfleury.fr --region eu-west-2
 aws s3 cp _site/en/index.html s3://kine-valfleury.fr/en/ --region eu-west-2
 aws s3 cp _site/fonts s3://kine-valfleury.fr/fonts --region eu-west-2 --recursive
+aws s3 cp _site/node_modules s3://kine-valfleury.fr/node_modules --region eu-west-2 --recursive
 
 aws s3 cp _site/feed.xml s3://kine-valfleury.fr --region eu-west-2
 aws s3 cp _site/sitemaps.xml s3://kine-valfleury.fr --region eu-west-2
@@ -16,16 +17,20 @@ function gzip_and_upload {
     aws s3 cp tmp/$1 s3://kine-valfleury.fr/$3/ --region eu-west-2 --content-type=$2 --cache-control="max-age=604800" --content-encoding="gzip"
 }
 
-jsList=( bootstrap.min.js custom.js jquery.easing.min.js jquery.min.js require.js main.js )
+jsList=( custom.js )
 for js in "${jsList[@]}"
 do
-    gzip_and_upload $js "text/javascript" "js"
+    gzip_and_upload $js "text/javascript" "assets/js"
 done
 
-cssList=( bootstrap.min.css font-awesome.min.css style.css )
+gzip_and_upload "jquery.min.js" "text/javascript" "node_modules/jquery/dist/"
+gzip_and_upload "popper.min.js" "text/javascript" "node_modules/popper.js/dist/umd"
+gzip_and_upload "bootstrap.min.js" "text/javascript" "node_modules/bootstrap/dist/js"
+
+cssList=( main.css font-awesome.min.css )
 for css in "${cssList[@]}"
 do
-    gzip_and_upload $css "text/css" "css"
+    gzip_and_upload $css "text/css" "assets"
 done
 
 pngList=( caducee-transparent.png en.png fr.png favicon.png )
