5e34288da707e8c48508b20ebe97eaa3521f8448
remy <aremy>
2021-12-18 18:36:49 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ bundle exec jekyll build
 aws s3 cp _site/index.html s3://kine-valfleury.fr --region eu-west-2
 aws s3 cp _site/manifest.json s3://kine-valfleury.fr --region eu-west-2
 aws s3 cp _site/en/index.html s3://kine-valfleury.fr/en/ --region eu-west-2
-aws s3 cp _site/fonts s3://kine-valfleury.fr/fonts --region eu-west-2 --recursive --cache-control="max-age=31536000"
+aws s3 cp _site/webfonts s3://kine-valfleury.fr/webfonts --region eu-west-2 --recursive --cache-control="max-age=31536000"
 aws s3 cp _site/node_modules s3://kine-valfleury.fr/node_modules --region eu-west-2 --recursive --cache-control="max-age=31536000"
 
 aws s3 cp _site/feed.xml s3://kine-valfleury.fr --region eu-west-2
@@ -29,11 +29,13 @@ done
 
 gzip_and_upload "bootstrap.min.js" "text/javascript" "node_modules/bootstrap/dist/js"
 
-cssList=( main.css font-awesome.min.css )
+cssList=( main.css fontawesome.min.css solid.css)
 for css in "${cssList[@]}"
 do
     gzip_and_upload $css "text/css" "assets"
 done
+
+gzip_and_upload "main.css.map" "application/json" "assets"
 
 pngList=( en.png fr.png favicon.png caducee-48.png caducee-72.png caducee-96.png caducee-144.png caducee-168.png caducee-192.png)
 for img in "${pngList[@]}"
